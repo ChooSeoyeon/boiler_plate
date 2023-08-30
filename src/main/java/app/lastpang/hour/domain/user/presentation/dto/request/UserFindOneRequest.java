1be1dc3c28abd69d59dd916fe380d05e58c41151
ChooSeoYeon <choo000407@naver.com>
2023-08-31 04:05:27 +0900

    feat: 테스트용 api dto 구현

---
+++ src/main/java/app/lastpang/hour/domain/user/presentation/dto/request/UserFindOneRequest.java
@@ -0,0 +1,11 @@
+package app.lastpang.hour.domain.user.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserFindOneRequest {
+
+    private Long id;
+}
